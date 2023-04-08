7e48015afcac99455fc44f8651595ee4a318dac0
SyedYakhub <syedyakhub9966@gmail.com>
2023-04-08 11:13:49 +0530

    edited Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "yakhub_bucket" {
   bucket = "yakhub-test-bucket"
-  acl    = "private"
+  aws_s3_bucket_acl    = "private"
 
   tags = {
     Name        = "sample_bucket"
